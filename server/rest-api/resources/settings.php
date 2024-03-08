ba376231410263bd0b0e84a2be0110c1fd01cb4e
Arjan Bas <94793951+ArbaSoftware@users.noreply.github.com>
2024-03-08 13:50:01 +0100

    Content location

---
+++ server/rest-api/resources/settings.php
@@ -0,0 +1,9 @@
+<?php
+    $_settings = [
+        'dbhost' => '192.168.2.74',
+        'dbname' => 'ada5_prod',
+        'dbuser' => 'ada',
+        'dbpassword' => 'ada',
+        'contentdir' => '/content'
+    ];
+?>
